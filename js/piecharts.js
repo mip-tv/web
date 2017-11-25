500d97a3d4089ed86f8f1140f3277c55c4532f8a
Joe <joy1412@googlemail.com>
2017-11-25 22:31:58 +0100

    added a little window in maps

--- js/piecharts.js
+++ js/piecharts.js
@@ -8,7 +8,8 @@ var ctx = document.getElementsByClassName("myPieChart"),
     myPieChart = new Chart(ctx, {
         type: "pie",
         data: {
-            labels: ["Blue", "Red", "Yellow", "Green"],
+            labels: ["Blue", "Red", "Yellow", "Green"]
+,
             datasets: [{
                 data: [12.21, 15.58, 11.25, 8.32],
                 backgroundColor: ["#007bff", "#dc3545", "#ffc107", "#28a745"]
